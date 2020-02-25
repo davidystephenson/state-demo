c1bfea2961a356c7c2dc28dae3f1b23cbb8fe4eb
David Y. Stephenson <david@davidystephenson.com>
2020-02-25 15:19:00 +0100

    Cute dog

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 
 function App() {
   // const stateArray = useState('hello')
   // const value = stateArray[0] // 'hello'
   // const setValue = stateArray[1] // function () { // changes the value }
+
+  const [src, setSrc] = useState('')
+
+  async function getDog () {
+    const response = await axios.get('https://dog.ceo/api/breeds/image/random')
+    console.log('response test:', response)
+    console.log('response.data test:', response.data)
+    console.log('response.data.message test:', response.data.message)
+
+    setSrc(response.data.message)
+  }
+
+  const effect = () => getDog()
+
+  useEffect(effect, [])
 
   const [value, setValue] = useState('hello')
 
@@ -25,11 +41,9 @@ function App() {
     </button>
 
     <button
-      onClick={
-        () => console.log('inline arrow')
-      }
+      onClick={getDog}
     >
-      <img width='400px' height='200px' src='https://miro.medium.com/max/4000/1*qSe6kCCaR4HVBrwNOEjH0Q.jpeg' />
+      <img width='300px' height='200px' src={src} />
     </button>
   </main>
 }
